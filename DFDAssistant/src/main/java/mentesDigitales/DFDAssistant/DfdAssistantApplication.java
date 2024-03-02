db9858853a53fa7b2e27613d0fdfef3c67f7fe17
iescalada884 <ied884@alumnos.unican.es>
2024-03-02 10:05:11 +0100

    Añadido proyecto java

---
+++ DFDAssistant/src/main/java/mentesDigitales/DFDAssistant/DfdAssistantApplication.java
@@ -0,0 +1,13 @@
+package mentesDigitales.DFDAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfdAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfdAssistantApplication.class, args);
+	}
+
+}
---
+++ DFDAssistant/src/test/java/mentesDigitales/DFDAssistant/DfdAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package mentesDigitales.DFDAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfdAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
